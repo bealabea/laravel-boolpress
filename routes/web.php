bb9b59b9047ea8aac3cebaed7710231e57f42b02
bealabea <beatricemarin89@yahoo.it>
2022-03-11 16:13:28 +0100

    Creates Laravel Authentication

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'Admin\HomeController@index')->name('admin.home');
+
